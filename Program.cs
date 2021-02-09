35389e6f65147738b206a3a41e91df98a91be7ed
Josh Mann <jlmann72@gmail.com>
2021-02-09 08:07:02 -0500

    Created DbContext to connect to existing database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace SCMovies
 {
@@ -9,6 +10,27 @@ namespace SCMovies
         public string PrimaryDirector { get; set; }
         public int YearReleased { get; set; }
         public string Genre { get; set; }
+    }
+
+    // Define a database context for our SuncoastMovies database/
+    // It derives from DbContext so we get all the
+    // abilities of a database context from EF Core
+    class SuncoastMoviesContext : DbContext
+    {
+        // Define a Movies property that is a DbSet
+        public DbSet<Movie> Movies { get; set; }
+
+        // Define a method required by EF that will configure our connection 
+        // to the database.
+        // 
+        // DbContextOptionsBuilder is provided to us. We then tell that object
+        // that we want to connect to a PostgreS database named SuncoastMovies on
+        // our local machine.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=SuncoastMovies");
+        }
+
     }
 
     class Program
